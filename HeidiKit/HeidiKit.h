bbab4413c9d2dba8b4a4eb3ef29d5cbd3446c532
Yun-young LEE <lseeker@ShootingStar.local>
2014-08-09 10:36:09 +0900

    Initial Commit

---
+++ HeidiKit/HeidiKit.h
@@ -0,0 +1,19 @@
+//
+//  HeidiKit.h
+//  HeidiKit
+//
+//  Created by Yun-young LEE on 2014. 8. 9..
+//  Copyright (c) 2014년 inode.kr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HeidiKit.
+FOUNDATION_EXPORT double HeidiKitVersionNumber;
+
+//! Project version string for HeidiKit.
+FOUNDATION_EXPORT const unsigned char HeidiKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HeidiKit/PublicHeader.h>
+
+
